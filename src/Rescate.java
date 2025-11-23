159dc366144e9a1e57ac76f2ee57a90873dde4a0
Emanuel <emanuelvillar@outlook.com>
2025-11-23 17:24:31 +0100

    definir atributos y constructor en Rescate

--- src/Rescate.java
+++ src/Rescate.java
@@ -1,6 +1,13 @@
 public class Rescate implements Runnable{
 
-    public Rescate(){}
+    private Balsa embarcacion;
+    private Barco navio;
+
+    public Rescate(Balsa embarcacion, Barco navio) {
+        this.embarcacion = embarcacion;
+        this.navio = navio;
+    }
+
     @Override
     public void run() {
         while(true){}
