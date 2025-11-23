6945537a25de4d7fea6bb3f71c6a9a855e7abc3c
Emanuel <emanuelvillar@outlook.com>
2025-11-23 17:20:51 +0100

    implementar método rescatar en Barco

--- src/Barco.java
+++ src/Barco.java
@@ -8,5 +8,22 @@ public class Barco {
     public Barco(List<Pasajero> listaOcupantes) {
         this.listaOcupantes = new ArrayList<>(listaOcupantes);
     }
+    public  List<Pasajero> rescatar(int plazasSolicitadas) {
+        List<Pasajero> grupoSalvado = new ArrayList<>();
+        int contador = 0;
 
+        // Prioridades de 1 a 4
+        for (int nivel = 1; nivel <= 4 && contador < plazasSolicitadas; nivel++) {
+            for (int i = 0; i < listaOcupantes.size() && contador < plazasSolicitadas; i++) {
+                if (listaOcupantes.get(i).getNivelPrioridad() == nivel) {
+                    grupoSalvado.add(listaOcupantes.get(i));
+                    listaOcupantes.remove(i);
+                    i--;
+                    contador++;
+                }
+            }
+        }
+
+        return grupoSalvado;
+    }
 }
