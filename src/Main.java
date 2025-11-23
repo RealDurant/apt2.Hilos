53f08a7d6816cfd0333b5005288250614ce1721c
Emanuel <emanuelvillar@outlook.com>
2025-11-23 17:27:31 +0100

    generar lista de pasajeros en Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        //Completar Codigo
+        List<Pasajero> listaPasajeros = new ArrayList<>();
+        Random generador = new Random();
 
+        for (int codigo = 1; codigo <= 352; codigo++) {
+            int valorTipo = generador.nextInt(4);
+            String categoria;
+            int nivelPrioridad;
+            switch (valorTipo) {
+                case 0 -> { categoria = "Niño";       nivelPrioridad = 1; }
+                case 1 -> { categoria = "Adulto";     nivelPrioridad = 2; }
+                case 2 -> { categoria = "Anciano";    nivelPrioridad = 3; }
+                default -> { categoria = "Tripulacion"; nivelPrioridad = 4; }
+            }
+            listaPasajeros.add(new Pasajero(codigo, categoria, nivelPrioridad));
+        }
     }
 }
